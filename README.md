11a8a1cde3ccd173baf1f27f460889270174a46f
Rodrigo Ramos <lavander.solucioinesweb@gmail.com>
2018-04-19 12:14:04 -0300

    firts commit

---
+++ README.md
@@ -0,0 +1,1 @@
+esta es mi vos en la televisioooon 
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'rr',
+  packages = ['rr'], # this must be the same as the name above
+  version = '0.1',
+  description = 'repositorio con funciones reutilizables para django ',
+  author = 'Rodrigo Ramos',
+  author_email = 'alejandro@geekytheory.com',
+  url = 'https://github.com/RodrigoHernan/rr-django', # use the URL to the github repo
+  download_url = 'https://github.com/RodrigoHernan/rr-django/tarball/0.1',
+  keywords = ['testing', 'logging', 'example'],
+  classifiers = [],
+)
